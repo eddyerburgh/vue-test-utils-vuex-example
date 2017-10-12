14779aa9b9beb1d42494b57caaa1cdea082a7902
Edd Yerburgh <edward.yerburgh@gmail.com>
2017-10-12 20:59:06 +0100

    refactor: use jest functions instead of sinon

--- test/unit/specs/Actions.spec.js
+++ test/unit/specs/Actions.spec.js
@@ -1,46 +1,45 @@
-import { shallow, createLocalVue } from 'vue-test-utils'
-import sinon from 'sinon'
-import Vuex from 'vuex'
-import Actions from '../../../src/components/Actions'
+import { shallow, createLocalVue } from 'vue-test-utils';
+import Vuex from 'vuex';
+import Actions from '../../../src/components/Actions';
 
-const localVue = createLocalVue()
+const localVue = createLocalVue();
 
-localVue.use(Vuex)
+localVue.use(Vuex);
 
 describe('Actions.vue', () => {
-  let actions
-  let store
+  let actions;
+  let store;
 
   beforeEach(() => {
     actions = {
-      actionClick: sinon.stub(),
-      actionInput: sinon.stub()
-    }
+      actionClick: jest.fn(),
+      actionInput: jest.fn(),
+    };
     store = new Vuex.Store({
       state: {},
-      actions
-    })
-  })
+      actions,
+    });
+  });
 
   it('calls store action actionInput when input value is input and an input even is fired', () => {
-    const wrapper = shallow(Actions, { store, localVue })
-    const input = wrapper.find('input')
-    input.element.value = 'input'
-    input.trigger('input')
-    expect(actions.actionInput.calledOnce).toBe(true)
-  })
+    const wrapper = shallow(Actions, { store, localVue });
+    const input = wrapper.find('input');
+    input.element.value = 'input';
+    input.trigger('input');
+    expect(actions.actionInput).toHaveBeenCalled();
+  });
 
   it('does not call store action actionInput when input value is not input and an input even is fired', () => {
-    const wrapper = shallow(Actions, { store, localVue })
-    const input = wrapper.find('input')
-    input.element.value = 'not input'
-    input.trigger('input')
-    expect(actions.actionInput.calledOnce).toBe(false)
-  })
+    const wrapper = shallow(Actions, { store, localVue });
+    const input = wrapper.find('input');
+    input.element.value = 'not input';
+    input.trigger('input');
+    expect(actions.actionInput).not.toHaveBeenCalled();
+  });
 
   it('calls store action actionClick when button is clicked', () => {
-    const wrapper = shallow(Actions, { store, localVue })
-    wrapper.find('button').trigger('click')
-    expect(actions.actionClick.calledOnce).toBe(true)
-  })
-})
+    const wrapper = shallow(Actions, { store, localVue });
+    wrapper.find('button').trigger('click');
+    expect(actions.actionClick).toHaveBeenCalled();
+  });
+});
--- test/unit/specs/Getters.spec.js
+++ test/unit/specs/Getters.spec.js
@@ -1,36 +1,35 @@
-import Vue from 'vue'
-import { shallow, createLocalVue } from 'vue-test-utils'
-import Vuex from 'vuex'
-import Actions from '../../../src/components/Getters'
+import { shallow, createLocalVue } from 'vue-test-utils';
+import Vuex from 'vuex';
+import Actions from '../../../src/components/Getters';
 
-const localVue = createLocalVue()
+const localVue = createLocalVue();
 
-localVue.use(Vuex)
+localVue.use(Vuex);
 
 describe('Getters.vue', () => {
-  let getters
-  let store
+  let getters;
+  let store;
 
   beforeEach(() => {
     getters = {
       clicks: () => 2,
-      inputValue: () => 'input'
-    }
+      inputValue: () => 'input',
+    };
 
     store = new Vuex.Store({
-      getters
-    })
-  })
+      getters,
+    });
+  });
 
   it('Renders state.inputValue in first p tag', () => {
-    const wrapper = shallow(Actions, { store, localVue })
-    const p = wrapper.find('p')
-    expect(p.text()).toBe(getters.inputValue())
-  })
+    const wrapper = shallow(Actions, { store, localVue });
+    const p = wrapper.find('p');
+    expect(p.text()).toBe(getters.inputValue());
+  });
 
   it('Renders state.clicks in second p tag', () => {
-    const wrapper = shallow(Actions, { store, localVue })
-    const p = wrapper.findAll('p').at(1)
-    expect(p.text()).toBe(getters.clicks().toString())
-  })
-})
+    const wrapper = shallow(Actions, { store, localVue });
+    const p = wrapper.findAll('p').at(1);
+    expect(p.text()).toBe(getters.clicks().toString());
+  });
+});
